4f3d61c6dfc616608964cbeb396a4c623e66a8bd
cavster <colm.m.cavanagh@gmail.com>
2019-03-18 13:21:28 +0000

    [CC]Updated for call to pay-api

--- app/model/payapi/SpjRequest.scala
+++ app/model/payapi/SpjRequest.scala
@@ -2,8 +2,7 @@ package model.payapi
 
 import play.api.libs.json.{Json, OFormat, OWrites}
 
-final case class SpjRequest(mibRef: String, amountInPence: Int, returnUrl: String = "http://www.gov.uk",
-                            backUrl: String = "http://www.gov.uk", traderAddress: String, descriptionMerchandise: String)
+final case class SpjRequest(mibReference: String, amountInPence: Int, traderDetails: String, merchandiseDetails: String)
 
 object SpjRequest {
   implicit val journeyRequestWrites: OWrites[SpjRequest] = Json.writes[SpjRequest]
